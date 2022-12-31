2acccd50008a6610c91f968892af7fb5f30903c6
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-31 03:27:34 -0300

    more math header

---
+++ src/more_math.h
@@ -0,0 +1,13 @@
+#ifndef MORE_MATH_H
+#define MORE_MATH_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+int mm_gcd(int, int);
+int mm_lcm(int, int);
+bool mm_is_perfect_square(int);
+int mm_find_higher_perfect_square(int);
+int mm_find_square_complement(int, int);
+
+#endif
