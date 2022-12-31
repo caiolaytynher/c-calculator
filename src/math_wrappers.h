9406108ad44c62342aa2d27fc7fbf6a7a1135378
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-31 03:26:00 -0300

    new wrappers header

---
+++ src/math_wrappers.h
@@ -0,0 +1,17 @@
+#ifndef MATH_WRAPPERS_C
+#define MATH_WRAPPERS_C
+
+#include "quadratic.h"
+#include <stdio.h>
+
+double mw_sum();
+double mw_sub();
+double mw_mul();
+double mw_div();
+double mw_pow();
+double mw_mean();
+double mw_max();
+double mw_min();
+QuadAns wm_quadratic();
+
+#endif
