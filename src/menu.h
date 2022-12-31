edef3bdfaf9d0b33d45a967bc5c23ceb7e068bc5
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-31 03:26:50 -0300

    added enum of possible options

--- src/menu.h
+++ src/menu.h
@@ -1,6 +1,18 @@
 #ifndef MENU_H
 #define MENU_H
 
+typedef enum {
+  ADDITION = 1,
+  SUBTRACTION,
+  MULTIPLICATION,
+  DIVISION,
+  POWER,
+  MEAN,
+  MAX,
+  MIN,
+  QUADRATIC,
+} MenuOption;
+
 int main_menu();
 
 #endif
